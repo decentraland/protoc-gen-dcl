fde541d1784d1bd356148b60ed39873db80e84fb
menduz <github@menduz.com>
2022-01-07 14:41:35 -0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,57 @@
+PROTOBUF_VERSION = 3.19.1
+PROTOC ?= protoc
+UNAME := $(shell uname)
+PROTO_FILES := $(wildcard src/*.proto)
+export PATH := node_modules/.bin:/usr/local/include/:protoc3/bin:$(PATH)
+
+ifneq ($(CI), true)
+LOCAL_ARG = --local --verbose --diagnostics
+endif
+
+ifeq ($(UNAME),Darwin)
+PROTOBUF_ZIP = protoc-$(PROTOBUF_VERSION)-osx-x86_64.zip
+else
+PROTOBUF_ZIP = protoc-$(PROTOBUF_VERSION)-linux-x86_64.zip
+endif
+
+install_compiler:
+	@# remove local folder
+	rm -rf protoc3 || true
+
+	@# Make sure you grab the latest version
+	curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$(PROTOBUF_VERSION)/$(PROTOBUF_ZIP)
+
+	@# Unzip
+	unzip $(PROTOBUF_ZIP) -d protoc3
+	@# delete the files
+	rm $(PROTOBUF_ZIP)
+
+	@# move protoc to /usr/local/bin/
+	chmod +x protoc3/bin/protoc
+
+install: install_compiler
+	npm install
+	npm i -S google-protobuf@$(PROTOBUF_VERSION)
+	npm i -S @types/google-protobuf@latest
+
+test:
+	node_modules/.bin/jest --detectOpenHandles --colors --runInBand $(TESTARGS) --coverage
+
+test-watch:
+	node_modules/.bin/jest --detectOpenHandles --colors --runInBand --watch $(TESTARGS) --coverage
+
+build:
+	@rm -rf dist || true
+	@mkdir -p dist
+	./node_modules/.bin/tsc -p tsconfig.json
+	chmod +x ./dist/bin.js
+
+codegen: build
+	${PROTOC} \
+		"--plugin=protoc-gen-dcl=./dist/bin.js" \
+		"-I=$(PWD)/test/codegen" \
+		"--js_out=import_style=commonjs,binary:$(PWD)/test/codegen" \
+		"--dcl_out=$(PWD)/test/codegen" \
+		"$(PWD)/test/codegen/s1.proto"
+
+.PHONY: build test codegen
---
+++ src/bin.ts
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+import { printFileDescriptorTSD } from "ts-protoc-gen/lib/ts/fileDescriptorTSD"
+import { ExportMap } from "ts-protoc-gen/lib/ExportMap"
+import { replaceProtoSuffix, withAllStdIn, getParameterEnums, throwError } from "ts-protoc-gen/lib/util"
+import { CodeGeneratorRequest, CodeGeneratorResponse } from "google-protobuf/google/protobuf/compiler/plugin_pb"
+import { FileDescriptorProto } from "google-protobuf/google/protobuf/descriptor_pb"
+import { generateDclRpcService } from "./codegen/dcl-rpc"
+
+/**
+ * This is the ProtoC compiler plugin.
+ *
+ * The Protocol Buffers Compiler can be extended to [support new languages via plugins](https://developers.google.com/protocol-buffers/docs/reference/other).
+ * A plugin is just a program which reads a CodeGeneratorRequest protocol buffer from standard input
+ * and then writes a CodeGeneratorResponse protocol buffer to standard output.
+ * These message types are defined in [plugin.proto](https://github.com/google/protobuf/blob/master/src/google/protobuf/compiler/plugin.proto).
+ *
+ */
+withAllStdIn((inputBuff: Buffer) => {
+  try {
+    const typedInputBuff = new Uint8Array(inputBuff.length)
+    typedInputBuff.set(inputBuff)
+
+    const codeGenRequest = CodeGeneratorRequest.deserializeBinary(typedInputBuff)
+    const codeGenResponse = new CodeGeneratorResponse()
+    codeGenResponse.setSupportedFeatures(CodeGeneratorResponse.Feature.FEATURE_PROTO3_OPTIONAL)
+    const exportMap = new ExportMap()
+    const fileNameToDescriptor: { [key: string]: FileDescriptorProto } = {}
+
+    // const parameter = codeGenRequest.getParameter()
+
+    codeGenRequest.getProtoFileList().forEach((protoFileDescriptor) => {
+      const fileDescriptorName = protoFileDescriptor.getName() || throwError("Missing file descriptor name")
+      fileNameToDescriptor[fileDescriptorName] = protoFileDescriptor
+      exportMap.addFileDescriptor(protoFileDescriptor)
+    })
+
+    codeGenRequest.getFileToGenerateList().forEach((fileName) => {
+      const outputFileName = replaceProtoSuffix(fileName)
+      const thisFile = new CodeGeneratorResponse.File()
+      thisFile.setName(outputFileName + ".d.ts")
+      thisFile.setContent(printFileDescriptorTSD(fileNameToDescriptor[fileName], exportMap))
+      codeGenResponse.addFile(thisFile)
+
+      generateDclRpcService(outputFileName, fileNameToDescriptor[fileName], exportMap).forEach((file) =>
+        codeGenResponse.addFile(file)
+      )
+    })
+
+    process.stdout.write(Buffer.from(codeGenResponse.serializeBinary().buffer))
+  } catch (err: any) {
+    console.error("protoc-gen-ts error: " + err.stack + "\n")
+    process.exit(1)
+  }
+})
